e4099265dd8fd3783b41a277acd1980e412586d7
mishael254 <mishaelmishantoz@gmail.com>
2023-11-29 17:56:26 +0300

    fixed the card and column contents to display in project details the tables remained though

--- src/views/Projectdetails.js
+++ src/views/Projectdetails.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import { Card, CardHeader, CardBody, Table } from "reactstrap";
+import { Card, CardHeader, CardBody, Table, Row, Col, CardTitle } from "reactstrap";
 
 function ProjectDetails() {
   const { projectName } = useParams();
@@ -22,41 +22,63 @@ function ProjectDetails() {
   }, [projectName]);
 
   if (projectDetails.length === 0) {
-    return <div>No project details available for {projectName}</div>;
+    return <div className="content">
+      <Row>
+      <Col md="6" >
+        <Card>
+          <CardHeader>
+          <div>No project details available for {projectName}</div>
+          </CardHeader>
+        </Card>
+      </Col>
+    </Row>
+    </div>;
+    
+    
   }
 
   const headers = Object.keys(projectDetails[0]);
 
   return (
-    <div>
-      <h1>{projectName} Details</h1>
-      <Card className="card-tasks">
-        <CardHeader>
-          <h6 className="title d-inline">Tasks({projectDetails.length})</h6>
-          <p className="card-category d-inline">today</p>
-        </CardHeader>
-        <CardBody>
-          <Table responsive>
-            <thead>
-              <tr>
-                {headers.map((header, index) => (
-                  <th key={index}>{header}</th>
+    <>
+    <div className="content">
+      <div className="react-notification-alert-container">
+        
+      </div>
+      <Row>
+        <Col md="12">
+          <Card>
+            <CardHeader>
+              <CardTitle tag="h4">{projectName} Projects</CardTitle>
+              <h6 className="title d-inline">Tasks({projectDetails.length}) </h6>
+              <p className="card-category d-inline">today</p>
+            </CardHeader>
+            <CardBody>
+            <Table responsive>
+          <thead>
+            <tr>
+              {headers.map((header, index) => (
+                <th key={index}>{header}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {projectDetails.map((detail, rowIndex) => (
+              <tr key={rowIndex}>
+                {headers.map((header) => (
+                  <td key={header}>{detail[header]}</td>
                 ))}
               </tr>
-            </thead>
-            <tbody>
-              {projectDetails.map((detail, rowIndex) => (
-                <tr key={rowIndex}>
-                  {headers.map((header) => (
-                    <td key={header}>{detail[header]}</td>
-                  ))}
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-        </CardBody>
-      </Card>
+            ))}
+          </tbody>
+        </Table>
+            </CardBody>
+          </Card>
+        </Col>
+        
+      </Row>
     </div>
+  </>
   );
 }
 
