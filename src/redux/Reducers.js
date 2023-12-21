651fdddc151a700ac1cdc8aef187010db6709ceb
mishael254 <mishaelmishantoz@gmail.com>
2023-12-21 11:05:38 +0300

    Make sure that the isLoading state in your Redux store is being updated correctly when the data is being fetched.

--- src/redux/Reducers.js
+++ src/redux/Reducers.js
@@ -17,33 +17,104 @@ const appSlice = createSlice({
   initialState,
   
   extraReducers: (builder) => {
+    //members datafetch
+    builder.addCase(fetchMembersAction.pending, (state) => {
+      state.isLoading = true;
+    });
     builder.addCase(fetchMembersAction.fulfilled, (state, action) => {
       state.members = action.payload;
+      state.isLoading = false;
       return state;
+    });
+    builder.addCase(fetchMembersAction.rejected, (state) => {
+      state.isLoading = false; // Set isLoading to false if there's an error fetching data
+    });
+
+    //deployments datafetch 
+    builder.addCase(fetchDeploymentsAction.pending, (state) => {
+      state.isLoading = true;
     });
     builder.addCase(fetchDeploymentsAction.fulfilled, (state, action) => {
       state.deployments = action.payload;
+      state.isLoading = false;
       return state;
+    });
+    builder.addCase(fetchDeploymentsAction.rejected, (state) => {
+      state.isLoading = false; // Set isLoading to false if there's an error fetching data
+    });
+
+
+    //feedback datafetch
+    builder.addCase(fetchFeedbacksAction.pending, (state) => {
+      state.isLoading = true;
     });
     builder.addCase(fetchFeedbacksAction.fulfilled, (state, action) => {
       state.feedbacks = action.payload;
+      state.isLoading = false;
       return state;
+    });
+    builder.addCase(fetchFeedbacksAction.rejected, (state) => {
+      state.isLoading = false; // Set isLoading to false if there's an error fetching data
+    });
+
+
+    //messages datafetch
+    builder.addCase(fetchMessagesAction.pending, (state) => {
+      state.isLoading = true;
     });
     builder.addCase(fetchMessagesAction.fulfilled, (state, action) => {
       state.messages = action.payload;
+      state.isLoading = false;
       return state;
+    });
+    builder.addCase(fetchMessagesAction.rejected, (state) => {
+      state.isLoading = false; // Set isLoading to false if there's an error fetching data
+    });
+
+
+
+    //statlogs datafetch
+    builder.addCase(fetchStatLogsAction.pending, (state) => {
+      state.isLoading = true;
     });
     builder.addCase(fetchStatLogsAction.fulfilled, (state, action) => {
       state.statLogs = action.payload;
+      state.isLoading = false;
       return state;
+    });
+    builder.addCase(fetchStatLogsAction.rejected, (state) => {
+      state.isLoading = false; // Set isLoading to false if there's an error fetching data
+    });
+
+
+    //projects datafetch
+    builder.addCase(fetchProjectsAction.pending, (state) => {
+      state.isLoading = true;
     });
     builder.addCase(fetchProjectsAction.fulfilled, (state, action) => {
       state.projects = action.payload;
+      state.isLoading = false;
       return state;
+    });
+    builder.addCase(fetchProjectsAction.rejected, (state) => {
+      state.isLoading = false; // Set isLoading to false if there's an error fetching data
+    });
+
+
+
+
+    
+    // playlists and lessons datafetch
+    builder.addCase(fetchPlaylistsAction.pending, (state) => {
+      state.isLoading = true;
     });
     builder.addCase(fetchPlaylistsAction.fulfilled, (state, action) => {
       state.playlists = action.payload;
+      state.isLoading = false;
       return state;
+    });
+    builder.addCase(fetchPlaylistsAction.rejected, (state) => {
+      state.isLoading = false; // Set isLoading to false if there's an error fetching data
     });
     
   },
