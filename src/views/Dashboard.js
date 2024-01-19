cd7d25dc075d845e7ead928e30c6c6a0ddef30ca
mishael254 <mishaelmishantoz@gmail.com>
2024-01-19 15:09:57 +0300

    create a progress bars to graphically display the progress of messages or topics listened

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -565,6 +565,8 @@ function Dashboard(props) {
                       <th>Topic</th>
                       <th>Listened</th>
                       <th>messages contained</th>
+                      <th>Progress</th>
+                      <th>actions</th>
                       
                     </tr>
                   </thead>
@@ -582,6 +584,22 @@ function Dashboard(props) {
                                 )
                               )}
                           </td>
+                          <td>
+                            <div className="progress-container progress-sm">
+                              <div className="progress">
+                              <span className="progress-value">25%</span>
+                                  <div className="progress-bar" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style={{width: '50%'}}></div>
+                              </div>
+                            </div>
+                          </td>
+                          <td>
+                            <button type="button" id="tooltip30547133" title="Refresh" className="btn-link btn-icon btn btn-success btn-sm">
+                              <i className="tim-icons icon-refresh-01"></i>
+                            </button>
+                            <button type="button" id="tooltip156899243" title="Delete" className="btn-link btn-icon btn btn-danger btn-sm">
+                              <i className="tim-icons icon-simple-remove"></i>
+                            </button>
+                          </td>
                         </tr>
                       ))
                     ) : (
@@ -594,116 +612,7 @@ function Dashboard(props) {
               </CardBody>
             </Card>
           </Col>
-          <div class="col-lg-12">
-            <div class="card">
-              <div class="card-header">
-                <div class="tools float-right">
-                  <div class="dropdown">
-                    <button type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="btn-icon dropdown-toggle btn btn-link"><i class="tim-icons icon-settings-gear-63"></i></button>
-                    <div tabindex="-1" role="menu" aria-hidden="true" class="dropdown-menu dropdown-menu-right">
-                      <a href="#pablo" tabindex="0" role="menuitem" class="dropdown-item">Action</a>
-                      <a href="#pablo" tabindex="0" role="menuitem" class="dropdown-item">Another action</a>
-                      <a href="#pablo" tabindex="0" role="menuitem" class="dropdown-item">Something else</a>
-                      <a href="#pablo" tabindex="0" role="menuitem" class="text-danger dropdown-item">Remove Data</a>
-                    </div>
-                  </div>
-                </div>
-                <h5 class="card-title">Management Table</h5>
-              </div>
-              <div class="card-body">
-                <div class="table-responsive ps">
-                  <table class="table">
-                    <thead class="text-primary">
-                      <tr>
-                        <th class="text-center">#</th>
-                        <th>Name</th>
-                        <th>Job Position</th>
-                        <th>Milestone</th>
-                        <th class="text-right">Salary</th>
-                        <th class="text-right">Actions</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td class="text-center">
-                          <div class="photo"><img alt="..." src="/black-dashboard-pro-react/static/media/tania.bc44d816.jpg"></img>
-                          </div>
-                        </td>
-                        <td>Tania Mike</td>
-                        <td>Develop</td>
-                        <td class="text-center">
-                          <div class="progress-container progress-sm">
-                            <div class="progress">
-                              <span class="progress-value">25%</span>
-                                <div class="progress-bar" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="width: 25%;"></div>
-                            </div>
-                          </div>
-                        </td>
-                        <td class="text-right">€ 99,225</td>
-                        <td class="text-right">
-                          <button type="button" id="tooltip618296632" title="Refresh" class="btn-link btn-icon btn-neutral btn btn-success btn-sm">
-                            <i class="tim-icons icon-refresh-01"></i>
-                          </button>
-                          <button type="button" id="tooltip707467505" title="Delete" class="btn-link btn-icon btn-neutral btn btn-danger btn-sm">
-                            <i class="tim-icons icon-simple-remove"></i>
-                          </button>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td class="text-center">
-                          <div class="photo">
-                            <img alt="..." src="/black-dashboard-pro-react/static/media/robi.ed6d75d2.jpg"></img>
-                          </div>
-                        </td>
-                        <td>John Doe</td>
-                        <td>CEO</td>
-                        <td class="text-center">
-                          <div class="progress-container progress-sm">
-                            <div class="progress">
-                              <span class="progress-value">77%</span>
-                                <div class="progress-bar" role="progressbar" aria-valuenow="77" aria-valuemin="0" aria-valuemax="100" style="width: 77%;">
-                                </div>
-                            </div>
-                          </div>
-                        </td>
-                        <td class="text-right">€ 89,241</td>
-                        <td class="text-right">
-                          <button type="button" id="tooltip216846074" title="Refresh" class="btn-link btn-icon btn-neutral btn btn-success btn-sm">
-                            <i class="tim-icons icon-refresh-01"></i>
-                          </button>
-                          <button type="button" id="tooltip391990405" title="Delete" class="btn-link btn-icon btn-neutral btn btn-danger btn-sm">
-                            <i class="tim-icons icon-simple-remove"></i>
-                          </button>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td class="text-center"><div class="photo">
-                          <img alt="..." src="/black-dashboard-pro-react/static/media/lora.5084ee01.jpg"></img>
-                    </div>
-                    </td>
-                    <td>Alexa Mike</td>
-                    <td>Design</td>
-                    <td class="text-center">
-                      <div class="progress-container progress-sm">
-                        <div class="progress">
-                          <span class="progress-value">41%</span>
-                            <div class="progress-bar" role="progressbar" aria-valuenow="41" aria-valuemin="0" aria-valuemax="100" style="width: 41%;">
-                            </div>
-                          </div>
-                        </div>
-                        </td><td class="text-right">€ 92,144</td>
-                        <td class="text-right">
-                          <button type="button" id="tooltip191500186" title="Refresh" class="btn-link btn-icon btn-neutral btn btn-success btn-sm">
-                            <i class="tim-icons icon-refresh-01"></i>
-                          </button>
-                          <button type="button" id="tooltip320351170" title="Delete" class="btn-link btn-icon btn-neutral btn btn-danger btn-sm">
-                            <i class="tim-icons icon-simple-remove"></i>
-                          </button>
-                        </td>
-                    </tr>
-                    <tr>
-                    <td class="text-center"><div class="photo"><img alt="..." src="/black-dashboard-pro-react/static/media/jana.918ef8bd.jpg"></img></div></td><td>Jana Monday</td><td>Marketing</td><td class="text-center"><div class="progress-container progress-sm"><div class="progress"><span class="progress-value">50%</span><div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;"></div></div></div></td><td class="text-right">€ 49,990</td><td class="text-right"><button type="button" id="tooltip345411997" title="Refresh" class="btn-link btn-icon btn btn-success btn-sm"><i class="tim-icons icon-refresh-01"></i></button><button type="button" id="tooltip601343171" title="Delete" class="btn-link btn-icon btn btn-danger btn-sm"><i class="tim-icons icon-simple-remove"></i></button></td></tr><tr><td class="text-center"><div class="photo"><img alt="..." src="/black-dashboard-pro-react/static/media/mike.da320cd2.jpg"></img></div></td><td>Paul Dickens</td><td>Develop</td><td class="text-center"><div class="progress-container progress-sm"><div class="progress"><span class="progress-value">100%</span><div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;"></div></div></div></td><td class="text-right">€ 69,201</td><td class="text-right"><button type="button" id="tooltip774891382" title="Refresh" class="btn-link btn-icon btn btn-success btn-sm"><i class="tim-icons icon-refresh-01"></i></button><button type="button" id="tooltip949929353" title="Delete" class="btn-link btn-icon btn btn-danger btn-sm"><i class="tim-icons icon-simple-remove"></i></button></td></tr><tr><td class="text-center"><div class="photo"><img alt="..." src="/black-dashboard-pro-react/static/media/emilyz.48a62490.jpg"></img></div></td><td>Manuela Rico</td><td>Manager</td><td class="text-center"><div class="progress-container progress-sm"><div class="progress"><span class="progress-value">15%</span><div class="progress-bar" role="progressbar" aria-valuenow="15" aria-valuemin="0" aria-valuemax="100" style="width: 15%;"></div></div></div></td><td class="text-right">€ 99,201</td><td class="text-right"><button type="button" id="tooltip30547133" title="Refresh" class="btn-link btn-icon btn btn-success btn-sm"><i class="tim-icons icon-refresh-01"></i></button><button type="button" id="tooltip156899243" title="Delete" class="btn-link btn-icon btn btn-danger btn-sm"><i class="tim-icons icon-simple-remove"></i></button></td></tr></tbody></table><div class="ps__rail-x" style="left: 0px; bottom: 0px;"><div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps__rail-y" style="top: 0px; right: 0px;"><div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 0px;"></div></div></div></div></div></div>
-         
+          
         </Row>
       </div>
     </>
