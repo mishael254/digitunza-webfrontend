d9d7fbcc6850814a4fe0cab37f68d086c9bd4afb
mishael <mishaelmishantoz@gmail.com>
2024-03-20 16:29:54 +0300

    modify the on collapse logo in landing component

--- src/components/Navbars/DemoNavbar.js
+++ src/components/Navbars/DemoNavbar.js
@@ -81,7 +81,7 @@ class DemoNavbar extends React.Component {
                       <Link to="/">
                         <img
                           alt="..."
-                          src={require("assets/img/brand/argon-react.png")}
+                          src={require("assets/img/apple-icon.png")}
                         />
                       </Link>
                     </Col>
